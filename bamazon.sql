6f12bc284fcf3161b0fee22e71df35f1711ad997
Emily Lau <emilylau5@gmail.com>
2017-10-23 17:43:22 -0700

    one done

--- bamazon.sql
+++ bamazon.sql
@@ -4,8 +4,8 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
-CREATE TABLE products(
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+CREATE TABLE productTable(
+  item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(50) NOT NULL,
   department_name VARCHAR(30) NOT NULL,
   price DECIMAL(10,2) NOT NULL, 
@@ -13,11 +13,11 @@ CREATE TABLE products(
   PRIMARY KEY(item_id) 
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO productTable (product_name, department_name, price, stock_quantity)
 VALUES ("lotion", "beauty", 5.99, 10);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO productTable (product_name, department_name, price, stock_quantity)
 VALUES ("water bottles", "drinks", .99, 300);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO productTable (product_name, department_name, price, stock_quantity)
 VALUES ("black clock", "home", 10.50, 20);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,49 @@ var connection = mysql.createConnection({
 
 var inquirer = require('inquirer');
 
+function start() {
+
+  connection.query("SELECT * FROM productTable", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + ") " + res[i].product_name + " || " + res[i].department_name + " || " + res[i].price + " || " + res[i].stock_quantity);
+    }
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the ID of the product you would like to buy?",
+        name: "productID"
+      },
+      {
+        type: "input",
+        message: "How many would you like to buy?",
+        name: "stockSubtract"
+      }
+    ]).then(function(resp){
+      var IDGrab = resp.productID - 1;
+      var subThis =  resp.stockSubtract
+      var priceTotal = res[IDGrab].price * resp.stockSubtract
+      if (res[IDGrab].stock_quantity > resp.stockSubtract) {
+        var newStock = (res[IDGrab].stock_quantity - resp.stockSubtract);
+        connection.query("UPDATE productTable SET ? WHERE ?",
+        [
+          {
+            stock_quantity: newStock
+          },
+          {
+            item_id: resp.productID
+          }
+        ],
+        function(err, respo) {
+          console.log("Your total today will be $" + priceTotal)
+          start();
+        }) //end connection and function
+      }
+      else {
+        console.log("Insufficient quantity!")
+        start()
+      }
+    }) //then end
+  }) //connection end
+} //start() end
+
+start();
